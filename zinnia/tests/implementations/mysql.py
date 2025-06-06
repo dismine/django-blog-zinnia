3d4646478f4238d498fd66ce0e5cb8f81243ad33
Roman Telezhynskyi <dismine@gmail.com>
2025-06-06 17:59:09 +0300

    Fix code style.

--- zinnia/tests/implementations/mysql.py
+++ zinnia/tests/implementations/mysql.py
@@ -1,15 +1,16 @@
 """Settings for testing zinnia on MySQL"""
+
 from zinnia.tests.implementations.settings import *  # noqa
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zinnia',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'TEST': {
-            'CHARSET': 'utf8',
-            'COLLATION': 'utf8_general_ci',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "zinnia",
+        "USER": "root",
+        "HOST": "localhost",
+        "TEST": {
+            "CHARSET": "utf8",
+            "COLLATION": "utf8_general_ci",
+        },
     }
 }
--- zinnia/tests/implementations/postgres.py
+++ zinnia/tests/implementations/postgres.py
@@ -1,11 +1,12 @@
 """Settings for testing zinnia on Postgres"""
+
 from zinnia.tests.implementations.settings import *  # noqa
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'zinnia',
-        'USER': 'postgres',
-        'HOST': 'localhost'
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "zinnia",
+        "USER": "postgres",
+        "HOST": "localhost",
     }
 }
